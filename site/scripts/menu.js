1c4bf17f0532ba1e634dbd1f2a619091f74c5ead
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2018-09-03 08:15:27 -0300

    Criação da Página Inicial

--- site/scripts/menu.js
+++ site/scripts/menu.js
@@ -1,4 +1,13 @@
-function redimensionarHeader()
+function testarAncora()
 {
-	document.getElementById('a').style.width = parseInt(window.width) - 5 + "px";
+	if (window.innerWidth >= 1000)
+	{
+		document.getElementById('login').style.left = 600 + (window.innerWidth - 1000) / 2 + "px";
+		document.getElementById('listaNavegacao').style.left = 470 + (window.innerWidth - 1000) + "px";
+	}
+	else
+	{
+		document.getElementById('login').style.left = "600px";
+		document.getElementById('listaNavegacao').style.left = "470px";
+	}
 }

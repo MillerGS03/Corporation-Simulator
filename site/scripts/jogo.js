6a70a5b701ce01d1e6cab9c8c409333173406a3e
MillerGS03 <gumiller2003@gmail.com>
2018-09-13 06:57:01 -0300

    Dinheiro colocado na barra superior

--- site/scripts/jogo.js
+++ site/scripts/jogo.js
@@ -3,8 +3,10 @@ var ctx = null;
 
 var imgDinheiro = new Image();
 var imgNivel = new Image();
+var imgCalendario = new Image();
 imgDinheiro.src = "imagens/iconeDinheiro.png";
 imgNivel.src = "imagens/iconeNivel.png";
+imgCalendario.src = "imagens/iconeCalendario.png";
 
 
 function iniciar()
@@ -19,7 +21,7 @@ function iniciar()
 
 function BarraSuperior() {
 	this.nivel = 5;
-	this.xp = 20;
+	this.xp = 10;
 	this.maxXP = 80;
 	this.dinheiro = 2000;
 	this.dia = 1;
@@ -46,23 +48,40 @@ function BarraSuperior() {
 
 		// Desenha a barra de experiência
 		ctx.fillStyle = "#8e8e8e";
-		ctx.fillRect(88, 18, 200, 20);
-		ctx.strokeRect(88, 18, 200, 20);
-		var grd = ctx.createLinearGradient(0, 0, (this.xp / this.maxXP) * 200, 20)
-		grd.addColorStop(0, "#4c98a5");
-		grd.addColorStop(1, "#4cbbce");
-		ctx.fillStyle = grd;
-		ctx.fillRect(88, 18, (this.xp / this.maxXP) * 200, 20);
-		ctx.strokeRect(88, 18, (this.xp / this.maxXP) * 200, 20);
+		roundRect(88, 18, 200, 20, {lowerRight: 10, upperRight:10}, true, true) // Retângulo base
+		var grd = null;
+		if (this.xp <= this.maxXP) // Retângulo de tamanho proporcional ao progresso naquele nível
+		{
+			grd = ctx.createLinearGradient(0, 0, (this.xp / this.maxXP) * 200, 20)
+			grd.addColorStop(0, "#4c98a5");
+			grd.addColorStop(1, "#4cbbce");
+			ctx.fillStyle = grd;
+			roundRect(88, 18, (this.xp / this.maxXP) * 200, 20, {lowerRight: 10, upperRight: 10}, true, true);
+		}
+		else
+		{
+			grd = ctx.createLinearGradient(0, 0, 200, 20)
+			grd.addColorStop(0, "#4c98a5");
+			grd.addColorStop(1, "#4cbbce");
+			ctx.fillStyle = grd;
+			roundRect(88, 18, 200, 20, {lowerRight: 10, upperRight: 10}, true, true);
+		}
 		ctx.font = "bold 12pt Century Gothic";
+		ctx.textAlign = "center";
 		ctx.fillStyle = "White";
-		ctx.fillText(this.xp + "/" + this.maxXP + " xp", 158, 34)
+		ctx.fillText(this.xp + "/" + this.maxXP + " xp", 188, 34)
 
 		// Desenha o dinheiro
-		ctx.drawImage(imgDinheiro, 320, 9);
-		ctx.font = "bold 16pt Century Gothic";
+		ctx.drawImage(imgDinheiro, 420, 9);
+		ctx.font = "bold 15pt Century Gothic";
+		ctx.textAlign = "left";
 		ctx.fillStyle = "Green";
-		ctx.fillText(this.dinheiro + ",00", 363, 37)
+		ctx.fillText(this.dinheiro + ",00", 463, 36)
+
+		// Desenha o dia
+		ctx.drawImage(imgCalendario, 690, 9);
+		ctx.fillStyle = "White";
+		ctx.fillText(this.dia + "° dia", 733, 36);
 	}
 }
 
@@ -74,4 +93,34 @@ function desenharFundo()
 
 	ctx.fillStyle = grd;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+function roundRect(x, y, width, height, radius, fill, stroke) // Desenha um retângulo com bordas redondas
+{
+	var cornerRadius = { upperLeft: 0, upperRight: 0, lowerLeft: 0, lowerRight: 0 };
+    if (typeof stroke == "undefined") {
+        stroke = true;
+    }
+    if (typeof radius === "object") {
+        for (var side in radius) {
+            cornerRadius[side] = radius[side];
+        }
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(x + cornerRadius.upperLeft, y);
+    ctx.lineTo(x + width - cornerRadius.upperRight, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + cornerRadius.upperRight);
+    ctx.lineTo(x + width, y + height - cornerRadius.lowerRight);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - cornerRadius.lowerRight, y + height);
+    ctx.lineTo(x + cornerRadius.lowerLeft, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - cornerRadius.lowerLeft);
+    ctx.lineTo(x, y + cornerRadius.upperLeft);
+    ctx.quadraticCurveTo(x, y, x + cornerRadius.upperLeft, y);
+    ctx.closePath();
+    if (stroke) {
+        ctx.stroke();
+    }
+    if (fill) {
+        ctx.fill();
+    }
 }

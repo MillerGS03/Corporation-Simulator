969b36cbae8e389226c08886c406807a5c378625
Scherer <fsvicentin@hotmail.com>
2018-09-15 02:01:50 -0300

    Atualização do formulário Selecionar
    * Efeito dentro do select adicionado;
     *Função para adicionar opções no select adicionada;
    
    Obs: o botão "Carregar!" será modificado.

--- site/scripts/selecionar.js
+++ site/scripts/selecionar.js
@@ -5,14 +5,17 @@ var ctx;
 function iniciar(){
 	canvas = document.getElementById("meuCanvas");
 	ctx = canvas.getContext("2d");
-	roundRect(50, 100, 900, 500, {upperLeft: 50, upperRight:50, lowerLeft: 50, lowerRight:50}, true, true);
+    canvasSelect = document.getElementById("canvasSelect");
+    ctxSelect = canvasSelect.getContext("2d");
+	roundRect(50, 100, 900, 500, {upperLeft: 50, upperRight:50, lowerLeft: 50, lowerRight:50}, true, true, "white", null);
 	ctx.font = "bold 20pt Century Gothic";
 	ctx.fillStyle = "Black";
 	ctx.textAlign = "center";
 	ctx.fillText("Selecione o jogo desejado:", canvas.width/2, 200);
+    addOptions();
 }
 
-function roundRect(x, y, width, height, radius, fill, stroke) // Desenha um retângulo com bordas redondas
+function roundRect(x, y, width, height, radius, fill, stroke, cor, txt) // Desenha um retângulo com bordas redondas
 {
 	var cornerRadius = { upperLeft: 0, upperRight: 0, lowerLeft: 0, lowerRight: 0 };
     if (typeof stroke == "undefined") {
@@ -40,9 +43,54 @@ function roundRect(x, y, width, height, radius, fill, stroke) // Desenha um ret�
     }
     if (fill) {
     	var grd = ctx.createLinearGradient(x, y, width + 2000, 1300);
-    	grd.addColorStop(0, "White");
-    	grd.addColorStop(1, "Black");
+    	grd.addColorStop(0, cor);
+    	grd.addColorStop(1, cor);
     	ctx.fillStyle = grd;
         ctx.fill();
+        if(txt != null) {
+            ctx.font = "bold Century Gothic 10pt"
+            ctx.textAlign = "center";
+            ctx.fillStyle = "white";
+            ctx.fillText(txt, x + 95, y + 33);
+        }
 	}
+}
+function CriarEfeitoSelect(e)
+{
+    var height, rx, ry, width, x, xc, y, yc, w, h;
+    h = canvasSelect.height;
+    w = canvasSelect.width;
+    width = window.innerWidth;
+    height = window.innerHeight;
+    x = event.clientX;
+    y = event.clientY;
+    rx = w * x / width;
+    ry = h * y / height;
+
+    var grdSelect = ctxSelect.createRadialGradient(rx, ry, 1, rx, ry, w);
+    grdSelect.addColorStop(0, "rgb(190, 190, 190)");
+    grdSelect.addColorStop(1, "white");
+
+    ctxSelect.fillStyle = grdSelect;
+    ctxSelect.fillRect(0, 0, canvasSelect.width, canvasSelect.height)
+}
+
+function addOptions()
+{
+    var s = document.getElementById("select");
+    var numeroDeJogos = 3 //pegar do bd
+    for (var i = 1; i <= numeroDeJogos; i++)
+    {
+        var o = document.createElement("option")
+        o.text = "ola";
+        s.add(o);
+    }
+}
+
+function TestarOpcao()
+{
+    var s = document.getElementById("select");
+    if (s.selectedIndex > 0) {
+        roundRect(canvas.width/2 - 100, 470, 200, 50, {upperLeft: 15, upperRight:15, lowerLeft: 15, lowerRight:15}, true, true, "green", "Carregar!");
+    }
 }

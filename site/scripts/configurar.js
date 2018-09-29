963c311997a22192f2a2c662f6fa2bc1cb064fda
Scherer <fsvicentin@hotmail.com>
2018-09-29 12:44:36 -0300

    Implementação do AJAX ao informações.html
    Feita a partir de jQuery;
    - Menu do usuário agora move-se de acordo com a página;
    - Referências feitas para todos os itens do menu.
    
    Obs: Usar xampp para executar o site;
    consertar o footer o mais rápido possível.

--- site/scripts/configurar.js
+++ site/scripts/configurar.js
@@ -5,16 +5,12 @@ var ctx;
 function iniciarMenu(){
 	canvas = document.getElementById("meuCanvas");
 	ctx = canvas.getContext("2d");
-    canvasSelect = document.getElementById("canvasSelect");
-    ctxSelect = canvasSelect.getContext("2d");
-	roundRect(50, 100, 900, 500, {upperLeft: 50, upperRight:50, lowerLeft: 50, lowerRight:50}, true, true, "white", null);
+	roundRect(50, 1, 900, 1500, {upperLeft: 50, upperRight:50, lowerLeft: 50, lowerRight:50}, true, true, "white", null);
 	ctx.font = "bold 20pt Century Gothic";
 	ctx.fillStyle = "Black";
 	ctx.textAlign = "center";
-	ctx.fillText("Selecione o jogo desejado:", canvas.width/2, 200);
-    addOptions();
+	ctx.fillText("", canvas.width/2, 200);
 }
-
 function roundRect(x, y, width, height, radius, fill, stroke, cor, txt) // Desenha um retângulo com bordas redondas
 {
 	var cornerRadius = { upperLeft: 0, upperRight: 0, lowerLeft: 0, lowerRight: 0 };
@@ -54,48 +50,4 @@ function roundRect(x, y, width, height, radius, fill, stroke, cor, txt) // Desen
             ctx.fillText(txt, x + 95, y + 33);
         }
 	}
-}
-function CriarEfeitoSelect(e)
-{
-    var height, rx, ry, width, x, xc, y, yc, w, h;
-    h = canvasSelect.height;
-    w = canvasSelect.width;
-    width = window.innerWidth;
-    height = window.innerHeight;
-    x = event.clientX;
-    y = event.clientY;
-    rx = w * x / width;
-    ry = h * y / height;
-
-    var grdSelect = ctxSelect.createRadialGradient(rx, ry, 1, rx, ry, w);
-    grdSelect.addColorStop(0, "rgb(190, 190, 190)");
-    grdSelect.addColorStop(1, "white");
-
-    ctxSelect.fillStyle = grdSelect;
-    ctxSelect.fillRect(0, 0, canvasSelect.width, canvasSelect.height)
-}
-
-function addOptions()
-{
-    var s = document.getElementById("select");
-    var numeroDeJogos = 3 //pegar do bd
-    for (var i = 1; i <= numeroDeJogos; i++)
-    {
-        var o = document.createElement("option")
-        o.text = "ola";
-        s.add(o);
-    }
-}
-
-function TestarOpcao()
-{
-    var s = document.getElementById("select");
-    if (s.selectedIndex > 0) {
-        document.getElementById("btnCarregar").style.visibility = "visible";
-    }
-}
-
-function carregarJogo()
-{
-    window.open("jogo.html");
 }
--- site/scripts/info.js
+++ site/scripts/info.js
@@ -1,25 +1,63 @@
-var username = "Vinschers";										//acessar banco para pegar dados
-var rank = "# " + 45;
-	function ColocarDados() {
-		document.getElementById("user").innerHTML = username;
-		document.getElementById("rank").innerHTML = rank;
-	}
-	function testarAncora()
+var username;
+var rank;
+function AcessarDados() 
+{
+	username = "Vinschers";
+	rank = "# " + 45;
+}
+function ColocarDados() {
+	AcessarDados();
+	$("#user").text(username);
+	$("#rank").text(rank);
+	$("#conteudo").css("height", "900");
+}
+function testarAncora()
+{
+	$('#listaNavegacao').css("position", "relative");
+	if (window.innerWidth >= 3000)
 	{
-		document.getElementById('listaNavegacao').style.position = "relative";
-		if (window.innerWidth >= 3000)
-		{
-			document.getElementById('listaNavegacao').style.left = "2220px";
-			document.getElementById('conteudo').style.width = "2577px";
-		}
-		else if (window.innerWidth >= 1000)
-		{
-			document.getElementById('listaNavegacao').style.left = 470 + (window.innerWidth - 1000) + "px";
-			document.getElementById('conteudo').style.width = window.innerWidth - 423 + "px";
-		}
-		else
-		{
-			document.getElementById('listaNavegacao').style.left = "470px";
-			document.getElementById('conteudo').style.width = "577px";
-		}
+		$('#listaNavegacao').css("left", "2220px")
+		$('#conteudo').css("width", "2577px");
 	}
+	else if (window.innerWidth >= 1000)
+	{
+		$('#listaNavegacao').css("left", 470 + (window.innerWidth - 1000) + "px");
+		$('#conteudo').css("width", window.innerWidth - 423 + "px");
+	}
+	else
+	{
+		$('#listaNavegacao').css("left", "470px");
+		$('#conteudo').css("width", "577px");
+	}
+}
+function abrir(arq) {
+	$("#conteudo").empty();
+	$('#conteudo').load(arq);
+	comecar();
+}
+
+function comecar() 
+{
+	setTimeout(aux, 105);
+	var batata = 2;
+}
+
+function aux()
+{
+	iniciarMenu();
+	$("#conteudo").css("height", 1000 + "px");
+}
+$(document).ready(function() {
+	$(window).scroll(function () {
+		//if ($("#conteudo").height() > 900) {
+			//$("#footer").css("top", $("#conteudo").height() + "px");
+		    console.log($(window).scrollTop())
+		    if ($(window).scrollTop() > 90) {
+		      	$('#menu').addClass('menuOpcoes-fixed');
+		    }
+		    if ($(window).scrollTop() < 91) {
+		      	$('#menu').removeClass('menuOpcoes-fixed');
+		    }
+		//}
+	});
+});

89a1c3820ed07c2d8ac390ed8f1b3e22907fd808
Scherer <fsvicentin@hotmail.com>
2018-10-12 10:51:14 -0300

    Fim do grafico de estatisticas
    algumas funcionalidades adicionadas, como possibilidade de ver todos os ganhos da empresa.
    - Há um pequeno bug quando o gráfico se move pela primeira vez que ainda não foi consertado.

--- site/scripts/objetosJogo/menus/Estatisticas.js
+++ site/scripts/objetosJogo/menus/Estatisticas.js
@@ -1,6 +1,16 @@
 var valores = new Array();
+var valoresTotais = new Array();
+var datas = new Array();
 var atual = 0;
+var atualTudo = 0;
+var dataAtual = 0;
 var dias;
+var imgProx = new Image();
+var imgAnterior = new Image();
+var tudo = false;
+var updateDeGrafico = false;
+var vezes = 0;
+var primerioUpdateDeData = true;
 function Estatisticas()
 {
     this.width = 700;
@@ -14,15 +24,35 @@ function Estatisticas()
     this.btnFechar = new BotaoRetangular(this.x + this.width - 50, this.y + 10, 40, 40,
                              { upperLeft: 5, upperRight: 5, lowerLeft: 5, lowerRight: 5 }, 40, 40,
         "#232323", "#535353", null, null, "bold 18pt Century Gothic", "red", "X", false, true, false);
+
+    imgProx.src = '../imagens/funcionalidades/setaDireita.png';
+    imgAnterior.src = '../imagens/funcionalidades/setaEsquerda.png';
+    this.btnProx = new BotaoCircular(este.x + este.width - 200, este.y + 100, 30, 30, "white", "#f2f2f2", imgProx, imgProx, "18pt Century Gothic", "white", "", false, false, true);
+    this.btnAnterior = new BotaoCircular(este.x + 200, este.y + 100, 30, 30, "white", "#f2f2f2", imgAnterior, imgAnterior, "18pt Century Gothic", "white", "", false, false, true);
     this.btnFechar.onclick = function(e) {
         este.abrirFechar();
+    }
+    this.btnProx.onclick = function(e){
+        tudo = true;
+    }
+    this.btnAnterior.onclick = function(e){
+        tudo = false;
     }
 
     this.desenhar = function() {
 
         if (this.aberto)
         {
+            if (tudo){
+                este.btnAnterior.ativarInteracao();
+                este.btnProx.desativarInteracao();
+            }
+            else {
+                este.btnProx.ativarInteracao();
+                este.btnAnterior.desativarInteracao();
+            }
             var escala;
+            var escalaTudo;
             var X = este.x + 65;
             var Y = este.y + 450;
             ctx.save();
@@ -33,8 +63,16 @@ function Estatisticas()
             }
             var aux2 = aux + "";
             escala = aux2.length - 1;
+            for (var i = 0; i < valoresTotais.length; i++)
+                if (aux < valoresTotais[i])
+                    aux = valoresTotais[i];
+            aux2 = aux + "";
+            escalaTudo = aux2.length;
             desenharJanela();
-            desenharEixos(X, Y, escala);
+            if (!tudo)
+                desenharEixos(X, Y, escala);
+            else
+                desenharEixos(X, Y, escala);
             ctx.beginPath();
             ctx.moveTo(X, Y);
             var atual, atualY = Y, atualX = X;
@@ -48,11 +86,18 @@ function Estatisticas()
         {
             desativarBotoes();
             this.btnFechar.ativarInteracao();
+            this.btnProx.ativarInteracao();
+            this.btnAnterior.desativarInteracao();
+            iniciarDatas();
         }
         else
         {
             this.btnFechar.desativarInteracao();
             this.btnFechar.hovering = false;
+            this.btnProx.desativarInteracao();
+            this.btnProx.hovering = false;
+            this.btnAnterior.desativarInteracao();
+            this.btnAnterior.hovering = false;
             ativarBotoes();
         }
         atualizar();
@@ -75,6 +120,19 @@ function Estatisticas()
     
         este.btnFechar.desenhar();
         roundRect(este.x, este.y + 60, este.width, este.height - 60, {lowerLeft: 20, lowerRight: 20 }, true, true);
+
+        ctx.fillStyle = "Black";
+        ctx.font = "bold 18pt Century Gothic";
+        var situacao = "";
+        if (!tudo){
+            situacao = "Últimos 15 dias"
+            este.btnProx.desenhar();
+        }
+        else{
+            situacao = "Todo o período";
+            este.btnAnterior.desenhar();
+        }
+        ctx.fillText(situacao, este.x + este.width/2, este.y + 90, este.width - 5);
     }
     function desenharEixos(x, y, e)
     {
@@ -106,25 +164,41 @@ function Estatisticas()
         var xAtual;
         var yAtual;
         var atual;
-        dias = calendario.qtosDiasTemOMes[calendario.mes - 1];
         var aux3 = x;
-
         ctx.beginPath();
-        for (var i = 0; i <= dias; i++) {
-            aux3 += (i!=0?(i<11?16:21):5);
-            ctx.fillText(i, aux3, y);
-            ctx.stroke();
-            ctx.strokeStyle = "#4286f4";
-            atual = valores[i]/Math.pow(10, e);
-            yAtual = y-(34 * atual);
-            if (i != 0) {
-                xAtual = aux3; 
+        if (!tudo){
+            for (var i = 0; i < 15; i++) {
+                aux3 += (i!=0?40:15);
+                ctx.fillText(datas[i], aux3, y);
+                ctx.stroke();
+                ctx.strokeStyle = "#4286f4";
+                atual = valores[i]/Math.pow(10, e);
+                yAtual = y-(33 * atual);
+                if (i != 0) {
+                    xAtual = aux3; 
+                }
+                else {
+                    xAtual = x;
+                }
+                ctx.lineTo(xAtual, yAtual);
+                ctx.stroke();
             }
-            else {
-                xAtual = x;
+        }
+        else {
+            for(var i = 0; i < valoresTotais.length; i++) {
+                aux3 += 620/valoresTotais.length;
+                ctx.strokeStyle = "#4286f4";
+                atual = valoresTotais[i]/Math.pow(10, e);
+                yAtual = y-(33 * atual);
+                if (i != 0) {
+                    xAtual = aux3; 
+                }
+                else {
+                    xAtual = x;
+                }
+                ctx.lineTo(xAtual, yAtual);
+                ctx.stroke();
             }
-            ctx.lineTo(xAtual, yAtual);
-            ctx.stroke();
         }
         ctx.closePath();
 
@@ -150,13 +224,41 @@ function Estatisticas()
         ctx.restore();
     }
     this.adicionarValor = function (v) {
-        if (!isNaN(v))
-            valores[atual] = v;
-        if (atual >= dias){
-            for (var i = 0; i < dias; i++)
-                valores[i] = valores[i+1];
+        if (!isNaN(v)) {
+            if (atual >= 15) {
+                for (var i = 0; i < 15; i++)
+                    valores[i] = valores[i+1];
+                valores[atual] = v;
+            }
+            else {
+                valores[atual++] = v;
+            }
+            valoresTotais[atualTudo++] = v;
+            if (vezes == 14){
+                for(var i = 0; i < 14; i++){
+                    datas[i] = datas[i+1];
+                }
+                datas[14] = calendario.dia + "/" + calendario.mes;
+            }
+            else
+                vezes++;
         }
-        else
-            atual++;
+    }
+    function iniciarDatas()
+    {
+        var dia;
+        var mes;
+        for (var i = 0; i < 15; i++){
+            mes = calendario.mes;
+            dia = calendario.dia + (i - 1);
+            if (dia == calendario.qtosDiasTemOMes[calendario.mes - 1]){
+                dia = 1;
+                mes++;
+            }
+            else{
+                dia++;
+            }
+            datas[i] = dia + "/" + mes;
+        }
     }
 }

f719dae8b9507c223c83f3b5d2943576538c2690
MillerGS03 <gumiller2003@gmail.com>
2018-10-16 23:41:00 -0300

    Maior responsividade adicionada

--- site/scripts/index.js
+++ site/scripts/index.js
@@ -1,3 +1,7 @@
+function testarAncora()
+{
+	$("#fundoJogo").css("background-size", window.innerWidth * 1.2 / 1.77 > 700?"120vw":"1366px");
+}
 function abrir(arq) {
 	$("#conteudo").empty();
 	$('#conteudo').load(arq);

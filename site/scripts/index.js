e6d6bc10c15bf4495e3d19a940cd7ddabbf8ca47
MillerGS03 <gumiller2003@gmail.com>
2018-10-17 23:49:31 -0300

    Maior responsividade

--- site/scripts/index.js
+++ site/scripts/index.js
@@ -1,8 +1,23 @@
+var arquivoAberto = "";
+var fatorTamanho = 1;
+
 function testarAncora()
 {
+	fatorTamanho = window.innerWidth / 1600;
 	$("#fundoJogo").css("background-size", window.innerWidth * 1.2 / 1.77 > 700?"120vw":"1366px");
+	if (arquivoAberto != "html/home.html")
+		switch (arquivoAberto)
+		{
+			case "html/funcionalidades.html":
+				break;
+			case "html/"
+		}
+	else
+		$("#conteudo").css("transform", "");
 }
 function abrir(arq) {
 	$("#conteudo").empty();
 	$('#conteudo').load(arq);
+	arquivoAberto = arq;
+	testarAncora();
 }
